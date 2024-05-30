b5648fcf2219e8312575ae876898fdd2aec3c34f
DANNNN <danielemerald@ymail.com>
2024-05-30 13:23:23 +0700

    Added new project

--- src/data.js
+++ src/data.js
@@ -2,14 +2,13 @@ import responsive from "./assets/responsive.png";
 import javascript from "./assets/javascript.png";
 import fundamental from "./assets/fundamental.jpg";
 import html from "./assets/html.jpg";
-import metaIntro from "./assets/metaIntro.png"
-import metaJavascript from "./assets/metaJavascript.png"
-import metaVersionControl from "./assets/metaVersionControl.png"
-import metaHtmlCss from "./assets/metaHtmlCss.png"
-import metaReact from "./assets/metaReact.png"
-import metaAdvancedReact from "./assets/metaAdvancedReact.png"
-import metaUXUI from "./assets/metaUXUI.png"
-
+import metaIntro from "./assets/metaIntro.png";
+import metaJavascript from "./assets/metaJavascript.png";
+import metaVersionControl from "./assets/metaVersionControl.png";
+import metaHtmlCss from "./assets/metaHtmlCss.png";
+import metaReact from "./assets/metaReact.png";
+import metaAdvancedReact from "./assets/metaAdvancedReact.png";
+import metaUXUI from "./assets/metaUXUI.png";
 
 export const certificationData = [
   {
@@ -66,94 +65,109 @@ export const skillData = [
 ];
 
 export const projectData = [
-    {
-        id: 1,
-        thumbnail: 'caffeinline',
-        title: "Caffein Line",
-        description: "A landing page of an online cafe serving beverages and desserts.",
-        tools: ['HTML', 'SCSS', 'Tailwind', 'jQuery'],
-        visit: 'https://caffeinline.netlify.app/',
-        github: 'https://github.com/Pilvorm/Caffein-Line'
-    },
-    {
-        id: 2,
-        thumbnail: 'TributePageNew',
-        title: "Cordae Tribute Page (Reimagined)",
-        description: "A tribute page about one of my favorite hip-hop artist, Cordae.",
-        tools: ['HTML', 'SCSS', 'jQuery'],
-        visit: 'https://cordae-tribute-page-reimagined.vercel.app/',
-        github: 'https://github.com/Pilvorm/Cordae-Tribute-Page-Reimagined'
-    },
-    {
-        id: 3,
-        thumbnail: 'Markdown',
-        title: "Markdown Previewer",
-        description: "A tool to preview markdowns.",
-        tools: ['React', 'SCSS'],
-        visit: 'https://markdown-previewer-tool.vercel.app/',
-        github: 'https://github.com/Pilvorm/markdown-previewer'
-    },
-    {
-        id: 4,
-        thumbnail: 'Quote',
-        title: "Random Movie Quote Generator",
-        description: "Get a random quote from iconic movies!",
-        tools: ['HTML', 'SCSS', 'jQuery'],
-        visit: 'https://random-movie-quotes.vercel.app/',
-        github: 'https://github.com/Pilvorm/Random-Movie-Quote'
-    },
-    {
-        id: 5,
-        thumbnail: 'hishot',
-        title: "HISHOT 2022",
-        description: "Event landing page held by HIMTI Binus University. Appointed as web development staff.",
-        tools: ['HTML', 'CSS', 'Bootstrap', 'JS'],
-        visit: 'https://hishot.himtibinus.or.id/',
-        github: 'https://github.com/himtibinus'
-    },
-    {
-        id: 6,
-        thumbnail: 'techfest',
-        title: "TECHFEST 2022",
-        description: "Landing page of new event held by HIMTI Binus University. Appointed as coordinator of web development division.",
-        tools: ['HTML', 'CSS', 'Tailwind', 'React'],
-        visit: 'https://techfest.himtibinus.or.id',
-        github: 'https://github.com/himtibinus'
-    },
-    {
-        id: 7,
-        thumbnail: 'Documentation',
-        title: "C Language Documentation",
-        description: "A documentation of C programming language.",
-        tools: ['HTML', 'CSS', 'JS', 'Highlight.js'],
-        visit: 'https://c-language-documentation.vercel.app/',
-        github: 'https://github.com/Pilvorm/Technical-Documentation'
-    },
-    {
-        id: 8,
-        thumbnail: 'SurveyForm',
-        title: "Streaming Site Survey Form",
-        description: "A survey form of a streaming website.",
-        tools: ['HTML', 'CSS'],
-        visit: 'https://survey-form-ecru.vercel.app/',
-        github: 'https://github.com/Pilvorm/Survey-Form'
-    },
-    {
-        id: 9,
-        thumbnail: 'lego',
-        title: "Product Landing Page",
-        description: "A landing page of the video game LEGO Star Wars: The Skywalker Saga.",
-        tools: ['HTML', 'CSS'],
-        visit: 'https://legoproductlanding.netlify.app/',
-        github: 'https://github.com/Pilvorm/Product-Landing'
-    },
-    {
-        id: 10,
-        thumbnail: 'TributePageOld',
-        title: "Cordae Tribute Page (Legacy)",
-        description: "Older version of a tribute page about Cordae.",
-        tools: ['HTML', 'SCSS'],
-        visit: 'https://cordae-tribute-page.vercel.app/',
-        github: 'https://github.com/Pilvorm/Cordae-Tribute-Page'
-    },
+  {
+    id: 1,
+    thumbnail: "littlelemon",
+    title: "Little Lemon",
+    description:
+      "Landing page of a restaurant allowing online reservations.",
+    tools: ["React", "SCSS", "Bootstrap"],
+    visit: "https://little-lemon-chg.vercel.app/",
+    github: "https://github.com/Pilvorm/little-lemon",
+  },
+  {
+    id: 2,
+    thumbnail: "caffeinline",
+    title: "Caffein Line",
+    description:
+      "A landing page of an online cafe serving beverages and desserts.",
+    tools: ["HTML", "SCSS", "Tailwind", "jQuery"],
+    visit: "https://caffeinline.netlify.app/",
+    github: "https://github.com/Pilvorm/Caffein-Line",
+  },
+  {
+    id: 3,
+    thumbnail: "TributePageNew",
+    title: "Cordae Tribute Page (Reimagined)",
+    description:
+      "A tribute page about one of my favorite hip-hop artist, Cordae.",
+    tools: ["HTML", "SCSS", "jQuery"],
+    visit: "https://cordae-tribute-page-reimagined.vercel.app/",
+    github: "https://github.com/Pilvorm/Cordae-Tribute-Page-Reimagined",
+  },
+  {
+    id: 4,
+    thumbnail: "Markdown",
+    title: "Markdown Previewer",
+    description: "A tool to preview markdowns.",
+    tools: ["React", "SCSS"],
+    visit: "https://markdown-previewer-tool.vercel.app/",
+    github: "https://github.com/Pilvorm/markdown-previewer",
+  },
+  {
+    id: 5,
+    thumbnail: "Quote",
+    title: "Random Movie Quote Generator",
+    description: "Get a random quote from iconic movies!",
+    tools: ["HTML", "SCSS", "jQuery"],
+    visit: "https://random-movie-quotes.vercel.app/",
+    github: "https://github.com/Pilvorm/Random-Movie-Quote",
+  },
+  {
+    id: 6,
+    thumbnail: "hishot",
+    title: "HISHOT 2022",
+    description:
+      "Event landing page held by HIMTI Binus University. Appointed as web development staff.",
+    tools: ["HTML", "CSS", "Bootstrap", "JS"],
+    visit: "https://hishot.himtibinus.or.id/",
+    github: "https://github.com/himtibinus",
+  },
+  {
+    id: 7,
+    thumbnail: "techfest",
+    title: "TECHFEST 2022",
+    description:
+      "Landing page of new event held by HIMTI Binus University. Appointed as coordinator of web development division.",
+    tools: ["HTML", "CSS", "Tailwind", "React"],
+    visit: "https://techfest.himtibinus.or.id",
+    github: "https://github.com/himtibinus",
+  },
+  {
+    id: 8,
+    thumbnail: "Documentation",
+    title: "C Language Documentation",
+    description: "A documentation of C programming language.",
+    tools: ["HTML", "CSS", "JS", "Highlight.js"],
+    visit: "https://c-language-documentation.vercel.app/",
+    github: "https://github.com/Pilvorm/Technical-Documentation",
+  },
+  {
+    id: 9,
+    thumbnail: "SurveyForm",
+    title: "Streaming Site Survey Form",
+    description: "A survey form of a streaming website.",
+    tools: ["HTML", "CSS"],
+    visit: "https://survey-form-ecru.vercel.app/",
+    github: "https://github.com/Pilvorm/Survey-Form",
+  },
+  {
+    id: 10,
+    thumbnail: "lego",
+    title: "Product Landing Page",
+    description:
+      "A landing page of the video game LEGO Star Wars: The Skywalker Saga.",
+    tools: ["HTML", "CSS"],
+    visit: "https://legoproductlanding.netlify.app/",
+    github: "https://github.com/Pilvorm/Product-Landing",
+  },
+  // {
+  //   id: 11,
+  //   thumbnail: "TributePageOld",
+  //   title: "Cordae Tribute Page (Legacy)",
+  //   description: "Older version of a tribute page about Cordae.",
+  //   tools: ["HTML", "SCSS"],
+  //   visit: "https://cordae-tribute-page.vercel.app/",
+  //   github: "https://github.com/Pilvorm/Cordae-Tribute-Page",
+  // },
 ];
